a158486defd040eb97ce01492aff25f671c1a2da
GasanaJr <gasanadj2003@gmail.com>
2023-11-01 12:47:57 +0200

    ft(queue): add queue DS

---
+++ Queue.js
@@ -0,0 +1,25 @@
+function Queue() {
+  var collection = [];
+  this.print = function () {
+    console.log(collection);
+  };
+  this.enqueue = function (value) {
+    collection.push(value);
+  };
+
+  this.dequeue = function () {
+    return collection.shift();
+  };
+
+  this.front = function () {
+    return collection[0];
+  };
+
+  this.size = function () {
+    return collection.length;
+  };
+
+  this.isEmpty = function () {
+    return collection.length == 0;
+  };
+}
